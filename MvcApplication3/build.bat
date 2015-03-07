a2c47fcc0a7370cad9d9311c2e357e9f78f3522e
lchong0412 <lchong0412@hotmail.com>
2015-03-07 14:46:07 +1300

    asdfadf

---
+++ MvcApplication3/build.bat
@@ -0,0 +1,2 @@
+"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe" MvcApplication3.sln /p:configuration=debug /p:VisualStudioVersion=12.0
+pause
---
+++ MvcApplication3/gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function (grunt) {
+    'use strict';
+ 
+    //S2    
+    grunt.initConfig({
+        //S3:Open the package.json
+        pkg: grunt.file.readJSON('package.json'),
+ 
+        msbuild: {
+            dev: {
+                src: ['MvcApplication3.sln'],
+                options: {
+                    projectConfiguration: 'Debug',
+                    maxCpuCount: 1,
+                    buildParameters: {
+                        VisualStudioVersion: "12.0"
+                    }
+                }
+            }
+        },
+
+        //S5:Task for Minification
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'MvcApplication3/MvcApplication3/angular/modules/student/studentModule.js',
+                dest: 'MvcApplication3/MvcApplication3/angular/modules/student/studentModule.min.js'
+            }
+        }
+    });
+ 
+ 
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-msbuild');
+ 
+    //S7: the array of tasks
+    grunt.registerTask('default', ['uglify', 'msbuild']);
+};
